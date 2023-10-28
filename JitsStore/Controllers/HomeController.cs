612349211e48e45541f10d0753804d8a59bbe09a
DanhTai <danhtai97tb@gmail.com>
2023-10-28 23:58:20 +0700

    Update design

--- JitsStore/Controllers/HomeController.cs
+++ JitsStore/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace JitsStore.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
