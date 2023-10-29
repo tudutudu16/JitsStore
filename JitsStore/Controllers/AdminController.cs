63df13b838af1d833ebc4ef119b77960433f95a0
DanhTai <danhtai97tb@gmail.com>
2023-10-29 09:20:49 +0700

    Update design Admin

---
+++ JitsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JitsStore.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Admin()
+		{
+			return View();
+		}
+
+        public IActionResult Table()
+        {
+            return View();
+        }
+    }
+}
